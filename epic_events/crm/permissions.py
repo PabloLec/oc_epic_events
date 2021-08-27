acdc0e2ac800fbbfaa11df9343ac82538237657c
PabloLec <pablo.lecolinet@gmail.com>
2021-08-27 10:38:19 +0200

    Fix sales related permissions

--- epic_events/crm/permissions.py
+++ epic_events/crm/permissions.py
@@ -8,9 +8,31 @@ class IsInManagementTeam(permissions.BasePermission):
         return request.user.role.value == "management"
 
 
-class IsInSalesTeam(permissions.BasePermission):
+class IsClientSalesContact(permissions.BasePermission):
     def has_permission(self, request, view):
-        return request.user.role.value == "sales"
+        is_sales = request.user.role.pk == get_role_id_by_name(name="sales")
+        if "pk" not in view.kwargs:
+            return request.method in ["GET", "POST"] and is_sales
+        client = Client.objects.get(pk=view.kwargs["pk"])
+        return client.sales_contact == request.user
+
+
+class IsContractSalesContact(permissions.BasePermission):
+    def has_permission(self, request, view):
+        is_sales = request.user.role.pk == get_role_id_by_name(name="sales")
+        if "pk" not in view.kwargs:
+            return request.method in ["GET", "POST"] and is_sales
+        contract = Contract.objects.get(pk=view.kwargs["pk"])
+        return contract.client.sales_contact == request.user
+
+
+class IsEventSalesContact(permissions.BasePermission):
+    def has_permission(self, request, view):
+        is_sales = request.user.role.pk == get_role_id_by_name(name="sales")
+        if "pk" not in view.kwargs:
+            return request.method in ["GET", "POST"] and is_sales
+        event = Event.objects.get(pk=view.kwargs["pk"])
+        return event.contract.client.sales_contact == request.user
 
 
 class IsSupportContact(permissions.BasePermission):
--- epic_events/crm/views.py
+++ epic_events/crm/views.py
@@ -6,25 +6,50 @@ from .permissions import *
 
 
 class ClientViewSet(viewsets.ModelViewSet):
+    def list(self, request):
+        is_sales = request.user.role.pk == get_role_id_by_name(name="sales")
+        print(get_role_id_by_name(name="sales"), request.user.username)
+        if is_sales:
+            print("OK")
+            queryset = Client.objects.filter(sales_contact=request.user)
+        else:
+            queryset = Client.objects.all()
+
+        serializer = ClientSerializer(queryset, many=True)
+        return Response(serializer.data)
+
     queryset = Client.objects.all()
     serializer_class = ClientSerializer
     http_method_names = ["get", "post", "put", "delete"]
     filterset_fields = ("first_name", "last_name", "email")
-    permission_classes = (IsInManagementTeam | IsInSalesTeam,)
+    permission_classes = (IsInManagementTeam | IsClientSalesContact,)
 
 
 class ContractViewSet(viewsets.ModelViewSet):
+    def list(self, request):
+        is_sales = request.user.role.pk == get_role_id_by_name(name="sales")
+        if is_sales:
+            queryset = Contract.objects.filter(client__sales_contact=request.user)
+        else:
+            queryset = Contract.objects.all()
+
+        serializer = ContractSerializer(queryset, many=True)
+        return Response(serializer.data)
+
     queryset = Contract.objects.all()
     serializer_class = ContractSerializer
     http_method_names = ["get", "post", "put", "delete"]
     filterset_fields = ("client__first_name", "client__last_name", "client__email", "created_time", "amount")
-    permission_classes = (IsInManagementTeam | IsInSalesTeam,)
+    permission_classes = (IsInManagementTeam | IsEventSalesContact,)
 
 
 class EventViewSet(viewsets.ModelViewSet):
     def list(self, request):
+        is_sales = request.user.role.pk == get_role_id_by_name(name="sales")
         is_support = request.user.role.pk == get_role_id_by_name(name="support")
-        if is_support:
+        if is_sales:
+            queryset = Event.objects.filter(client__sales_contact=request.user)
+        elif is_support:
             queryset = Event.objects.filter(support_contact=request.user)
         else:
             queryset = Event.objects.all()
@@ -35,4 +60,4 @@ class EventViewSet(viewsets.ModelViewSet):
     serializer_class = EventSerializer
     http_method_names = ["get", "post", "put", "delete"]
     filterset_fields = ("client__first_name", "client__last_name", "client__email", "event_date")
-    permission_classes = (IsInManagementTeam | IsInSalesTeam | IsSupportContact,)
+    permission_classes = (IsInManagementTeam | IsEventSalesContact | IsSupportContact,)
